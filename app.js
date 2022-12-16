234078b966e27029f079be26b7d1ba798858594a
archanapnair14 <96192333+archanapnair14@users.noreply.github.com>
2022-12-16 18:15:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = new express();
+const fs = require("fs");
+app.use(express.json());
+const data = require("./dataset.json");
+
+//Get Hospital Datas
+app.get("/hospital", (req, res) => {
+  res.send(data);
+});
+//POST Hospial Data
+app.post("/hospital", (req, res) => {
+  data.push(req.body);
+  fs.writeFile("dataset.json", JSON.stringify(data), (err, resp) => {
+    if (err) {
+      res.send("Data can not return");
+    } else {
+      res.send("Data Added Successfully");
+    }
+  });
+});
+
+//PUT Request
+app.put("/hospital/:name", (req, res) => {
+  let name = req.params.name;
+  data.forEach((item) => {
+    if (item.HospitalName == name) {
+      item.PatientCount = req.body.PatientCount;
+      item.HospitalLocation = req.body.HospitalLocation;
+    }
+  });
+  fs.writeFile("dataset.json", JSON.stringify(data), (err, resp) => {
+    if (err) {
+      res.send("Data could not be updated");
+    } else {
+      res.send("Data updated");
+    }
+  });
+});
+
+//DELTE request
+app.delete("/hospital/:name", (req, res) => {
+  let name = req.params.name;
+  let value = data.filter((item) => item.HospitalName !== name);
+  fs.writeFile("dataset.json", JSON.stringify(value), (err, resp) => {
+    if (err) {
+      res.send("Data could not be deleted");
+    } else {
+      res.send("Data deleted");
+    }
+  });
+});
+app.listen(3000);
+console.log("Server Listening to port 3000");
